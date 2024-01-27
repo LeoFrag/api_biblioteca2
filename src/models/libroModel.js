b70842af71b8eef79371ae5a4ffca2fc4e84345d
fragherazzi90162 <90162@sistemas.frc.utn.edu.ar>
2024-01-27 20:29:39 -0300

    Test unitarios y de integracion

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-});
+mongoose.connect("mongodb://127.0.0.1:27017/biblioteca", {
+
+  useNewUrlParser: "true",
+  useUnifiedTopology: "true"
+
+})
 
 const LibroSchema = new mongoose.Schema({
   titulo: String,
